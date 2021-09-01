80196cdde9ebb1c5fad84bd3e409dff896da0677
anirudh8080 <83979912+anirudh8080@users.noreply.github.com>
2021-09-02 00:54:12 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -5,12 +5,11 @@
 class Translation(object):
     
     START_TEXT = """<b>Hey {}!!</b>
-<i>Am Just A Advance Auto Filter Bot....😉
+<i>Am Just A Advance Auto Filter Bot Created By Anirudh
 
-Just Add Me To Your Group And Channel And Connect Them And See My Pevers 🔥🔥😝
+I'm a Private Bot
 
-For More Details Click Help Button Below..
-@CrazyBotsz
+You can't add me to you group
 </i>"""    
     
     HELP_TEXT = """
@@ -48,11 +47,11 @@ For More Details Click Help Button Below..
     
     ABOUT_TEXT = """<b>➥ Name</b> : <code> Auto Filter Bot</code>
     
-<b>➥ Creator</b> : <b><i><a href="https://t.me/AlbertEinstein_TG">AlbertEinstein_TG</a></i></b>
+<b>➥ Creator</b> : <b><i><a href="https://t.me/rudhra3">Anirudh</a></i></b>
 
 <b>➥ Language</b> : <code>Python3</code>
 
 <b>➥ Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
 
-<b>➥ Source Code</b> : <i><a href="https://github.com/AlbertEinsteinTG/Adv-Auto-Filter-Bot">Click Me</a></i>
+<b>➥ Source Code</b> : <i><a href="https://github.com/anirudh8080/AutoFilterBotV1">Click Me</a></i>
 """
