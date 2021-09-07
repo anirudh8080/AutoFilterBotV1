66a58ffbb092d3dea03a8efcd9b42c059c5fd274
anirudh8080 <83979912+anirudh8080@users.noreply.github.com>
2021-09-07 19:08:05 +0530

    Update auto_filter.py

--- bot/plugins/auto_filter.py
+++ bot/plugins/auto_filter.py
@@ -184,13 +184,13 @@ async def auto_filter(bot: Client, update: Message):
                 if ((len(ibuttons)%2) == 0):
                     ibuttons.append(
                         [
-                            InlineKeyboardButton(f"⚜ {chat_name} ⚜", url=invite_link)
+                            InlineKeyboardButton(f"⚜ {chat_name} ⚜", url=t.me/NothingHere4U)
                         ]
                     )
 
                 else:
                     ibuttons[-1].append(
-                        InlineKeyboardButton(f"⚜ {chat_name} ⚜", url=invite_link)
+                        InlineKeyboardButton(f"⚜ {chat_name} ⚜", url=t.me/NothingHere4U)
                     )
                 
             for x in ibuttons:
